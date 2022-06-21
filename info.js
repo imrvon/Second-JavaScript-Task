54845adf66d1368ebe3ba62935f68c30b99d4f33
VoN <webdesignervon@gmail.com>
2022-06-21 20:32:30 +0100

    Made if more interactive with if statements

--- info.js
+++ info.js
@@ -1,27 +1,34 @@
 function Info() {
 
     let name = prompt("What is your name?");
-    console.log(`Okay. You're welcome ${name}`);
+    alert(`You're welcome ${name}`);
     let height = parseInt(prompt("What is your height?\nNOTE: Number only"));
     let country = prompt("What is your country?");
 
-    let newLine = "\r\n"
-    let msg = `Your name is ${name}.`
-    msg += newLine;
-    msg += `You're ${height}ft. tall`
-    msg += newLine;
-    msg += `And you have the rotten luck of being from ${country}.`
+    let short_msg = `Your name is ${name}.
+You're ${height}ft. tall.
+And you have the rotten luck of being from ${country}.
+Omo you're short and still from Nigeria. Only you.
+Na wa o.`
 
-    let new_msg = `Your name is ${name}.`
-    new_msg += newLine;
-    new_msg += `You're ${height}ft. tall`
-    new_msg += newLine;
-    new_msg += `At least you're from ${country}, not Nigeria.`
+let tall_msg = `Your name is ${name}.
+You're ${height}ft. tall.
+Nothing to brag about, dongo.
+Remember you're unfortunate enough to be from ${country}.
+So wipe that smirk off your face.`
 
-    if (country === "Nigeria") {
-        alert(msg);
+    let new_msg = `Your name is ${name}.
+You're ${height}ft. tall.
+Short o. Tall o. E no matter.
+At least you're from ${country}, not Nigeria.
+Trust me, it could be worse.`
+
+    if (country === "Nigeria" && height < 6) {
+        alert(short_msg);
+    } else if (country === "Nigeria" && height >= 6) {
+        alert(tall_msg);
     } else {
-        alert(new_msg)
+        alert(new_msg);
     }
 }
 
