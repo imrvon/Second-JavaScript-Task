5619b875bb796cd8c134153ebab3d6b174f94ac7
VoN <webdesignervon@gmail.com>
2022-06-22 10:24:18 +0100

    Changed direction from prompt to console.log

--- info.js
+++ info.js
@@ -1,39 +1,5 @@
-function Info() {
+let name = "VoN"
+let height = 5.9;
+let country = "Nigeria"
 
-    // Receive Input from users
-    let name = prompt("What is your name?");
-    alert(`You're welcome ${name}`);
-    let height = parseInt(prompt("What is your height?\nNOTE: Number only"));
-    let country = prompt("What is your country?");
-
-// If the user is from Nigeria and is short
-    let short_msg = `Your name is ${name}.
-You're ${height}ft. tall.
-And you have the rotten luck of being from ${country}.
-Omo you're short and still from Nigeria. Only you.
-Na wa o.`
-
-// If the user is from Nigeria and is tall
-let tall_msg = `Your name is ${name}.
-You're ${height}ft. tall.
-Nothing to brag about, dongo.
-Remember you're unfortunate enough to be from ${country}.
-So wipe that smirk off your face.`
-
-// If the user is not from Nigeria
-    let new_msg = `Your name is ${name}.
-You're ${height}ft. tall.
-Short o. Tall o. E no matter.
-At least you're from ${country}, not Nigeria.
-Trust me, it could be worse.`
-
-    if (country.toLowerCase === "Nigeria".toLowerCase && height < 6) {
-        alert(short_msg);
-    } else if (country.toLowerCase === "Nigeria".toLowerCase && height >= 6) {
-        alert(tall_msg);
-    } else {
-        alert(new_msg);
-    }
-}
-
-Info();
+console.log(name + ' ' + height + ' ' + country);
